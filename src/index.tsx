7b4c7a01b41b9faf996e4b45c99318df27de11c6
super-programmer <zkp593655843@163.com>
2019-08-08 11:08:38 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Hello from './components/Hello';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { EnthusiasmAction} from './actions/index';
+import Hello from './containers/Hello';
 import './index.css';
+import { enthusiasm } from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
-ReactDOM.render(
-    <Hello name="TypeScript" enthusiasmLevel={10} />,
-    document.getElementById('root')!
-);
+import { StoreState } from './types/index';
+const store = createStore<StoreState,EnthusiasmAction,any,any>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
 
+ReactDOM.render(
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
+);
 registerServiceWorker();
